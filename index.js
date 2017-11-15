c47476c4f63790d4400449d7121010b6f8145b3d
Adam Beyer <abeyerdev@gmail.com>
2017-11-15 10:37:06 -0600

    added functionality to handle comment replies and clieaned up logging

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ import snoowrap from 'snoowrap'
 import credentials from './config/credentials'
 import subreddits from './config/subreddits'
 
+const { userAgent, clientId, clientSecret, username, password } = credentials
+
 const r = new snoowrap({
-    userAgent: credentials.userAgent,
-    clientId: credentials.clientId,
-    clientSecret: credentials.clientSecret,
-    username: credentials.username,
-    password: credentials.password
+    userAgent,
+    clientId,
+    clientSecret,
+    username,
+    password
 });
 
-async function main() {        
-    while(true) {
-        for(let i = 0; i < subreddits.length; i++) {
-            await formatAndLogComments(subreddits[i])              
-            await sleep(2000)
+async function main() {
+    try {
+        while(true) {
+            for(let i = 0; i < subreddits.length; i++) {            
+                await findAndReplyToEligibleComments(subreddits[i])            
+            }
+            await sleep(5000)
         }
-    }
+    } catch(e) {
+        console.error(e)
+    }   
 }
 
-function validComment(comment) {
+// A valid comment would be in the form 'r/iamverysmart or /r/iamverysmart' 
+function isValidComment(comment) {
     const validHtml = comment.body_html.includes('<a href="/r/')
     const eligibleBody = comment.body.split(' ')[0];
     const validBody = eligibleBody.match(/\/?r\/[A-Za-z]{3,}/g)
@@ -31,19 +38,37 @@ async function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-async function formatAndLogComments(subredditName) {
-    r.getSubreddit(subredditName).getNewComments()
-        .filter(comment => validComment(comment))
-        .map((comment) => {
+async function findAndReplyToEligibleComments(subredditName) {
+    r.getSubreddit(subredditName)
+        .getNewComments()
+        .filter(comment => isValidComment(comment))
+        .map(comment => {
             return {
                 id: comment.id,
-                timestamp: comment.created,
-                body: comment.body,
-                html: comment.body_html,
-                author: comment.author,
-                upvotes: comment.ups 
+                subredditPosted: comment.body.split(' ')[0]
             }
-        }).then(console.log)   
+        })
+        .forEach(comment => { 
+            await createBotReply(comment.id, comment.subredditPosted) 
+        })
+}
+
+// Create a reply to an eligible comment with information
+// regarding whether or not the poted subreddit exists.
+async function createBotReply(commentId, subredditPosted) {
+    let replyText = ''
+
+    if(subredditExists(subredditPosted)) { 
+        replyText = 'this' 
+    } else { 
+        replyText = 'that' 
+    }
+    
+    return r.getComment(commentId).reply(replyText)
+}
+
+async function subredditExists(subredditName) {
+    return r.getSubreddit(subredditName) != null
 }
 
 main()
